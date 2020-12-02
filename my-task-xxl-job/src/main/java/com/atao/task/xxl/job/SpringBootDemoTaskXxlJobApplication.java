ce834772f21c767c58fe2db70f5ebd1d19186fff
atao <zts_75@163.com>
2020-12-02 11:51:35 +0800

    集成xxl-job

---
+++ my-task-xxl-job/src/main/java/com/atao/task/xxl/job/SpringBootDemoTaskXxlJobApplication.java
@@ -0,0 +1,17 @@
+package com.atao.task.xxl.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author atao
+ * @date 2020/12/2
+ */
+@SpringBootApplication
+public class SpringBootDemoTaskXxlJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoTaskXxlJobApplication.class, args);
+    }
+
+}
